b59a6f02da6af9e951db4730c8fee7a1f9e2800b
sean nguyen <seannguyennn04@gmail.com>
2025-07-01 18:26:42 -0700

    created functions that allow the user to reset, change the color using buttons, and erase (maybe can implement a feature where the user just inputs based on what they type so they could have different styles of color)

--- etchsketch.js
+++ etchsketch.js
@@ -1,3 +1,4 @@
+let color = "black"
 
 function populate(size){
     const board = document.querySelector(".container")
@@ -13,12 +14,10 @@ function populate(size){
 
     let amount = size * size
 
-
+    //we are going to set our colors here as well
     for(let i = 0; i < amount; i++){ 
         let square = document.createElement("div")
-        square.addEventListener("mouseover", ()=>{
-            square.style.background = getRandomHexColor()
-        })
+        square.addEventListener("mouseover", changeColor);
         // square.style.border = "1px solid black";
         board.appendChild(square)
     }
@@ -27,9 +26,33 @@ function populate(size){
 // populate(16);
 
 
+//we are going to have a function that will change the colors of our divs
+function changeColor(){
+    if(color.toLowerCase() === "rainbow"){
+        this.style.backgroundColor =  getRandomHexColor()
+    }else{ 
+        this.style.backgroundColor = color
+    }
+  
+}
+
+function getColor(input){ 
+    color = input
+}
 
 
 
+
+function resetBoard(){
+    const board = document.querySelector(".container")
+    const squares = board.querySelectorAll("div") //this is getting all the divs from the board 
+
+    squares.forEach(element => (element.style.backgroundColor = "white"));
+
+}
+
+
+//asks for user input
 function createSize(input){
 
     if(input < 1 || input > 100){
@@ -50,7 +73,9 @@ document.getElementById("create-sizes").addEventListener("click",
     }
 )
 
-//create a random color
+
+
+//this function will create the rainbow mode
 
 function getRandomHexColor() {
   // Generate a random number between 0 and 16777215 (which is 0xFFFFFF in decimal)
@@ -65,3 +90,4 @@ function getRandomHexColor() {
 
   return hexColor;
 }
+
